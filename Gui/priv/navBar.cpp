1d8ecad964b2194923ff3b30795d64173bdf578c
Ed-Visinheski <eduardo.cook-visinheski@city.ac.uk>
2025-11-22 22:11:09 +0100

    Added Icons to nav bar

--- Gui/priv/navBar.cpp
+++ Gui/priv/navBar.cpp
@@ -2,6 +2,10 @@
 #include <QPushButton>
 #include <QHBoxLayout>
 
+#include <filesystem>
+#include <iostream>
+namespace fs = std::filesystem;
+
 NavBar::NavBar(QWidget* parent)
     : QWidget(parent)
 {
@@ -9,14 +13,14 @@ NavBar::NavBar(QWidget* parent)
     setStyleSheet("background-color: #f0f0f0;");
 
     // Buttons A, B, C
-    auto btnA = new QPushButton("A", this);
-    auto btnB = new QPushButton("B", this);
-    auto btnC = new QPushButton("C", this);
+    auto homeButton = new QPushButton(QIcon(QString("icons/magnifying-glass_24px.png")), QString{}, this);
+    auto ticketButton = new QPushButton(QIcon("icons/ticket_24px.png"), QString{}, this);
+    auto idButton = new QPushButton(QIcon("icons/user_24px.png"), QString{}, this);
 
     // Optional: set uniform size for buttons
-    btnA->setFixedSize(80, 40);
-    btnB->setFixedSize(80, 40);
-    btnC->setFixedSize(80, 40);
+    homeButton->setFixedSize(80, 40);
+    ticketButton->setFixedSize(80, 40);
+    idButton->setFixedSize(80, 40);
 
     // Layout with equal spacing: stretch segments around and between buttons
     auto layout = new QHBoxLayout(this);
@@ -24,11 +28,11 @@ NavBar::NavBar(QWidget* parent)
     layout->setSpacing(0);
 
     layout->addStretch(1);
-    layout->addWidget(btnA, 0, Qt::AlignCenter);
+    layout->addWidget(homeButton, 0, Qt::AlignCenter);
     layout->addStretch(1);
-    layout->addWidget(btnB, 0, Qt::AlignCenter);
+    layout->addWidget(ticketButton, 0, Qt::AlignCenter);
     layout->addStretch(1);
-    layout->addWidget(btnC, 0, Qt::AlignCenter);
+    layout->addWidget(idButton, 0, Qt::AlignCenter);
     layout->addStretch(1);
 
     setLayout(layout);
--- Gui/pub/navBar.h
+++ Gui/pub/navBar.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <QWidget>
+#include <QIcon>
 
 class NavBar : public QWidget
 {
