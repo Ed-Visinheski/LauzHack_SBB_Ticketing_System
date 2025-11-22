d8284d1b6fd99efe1cfae875d906ad7b70be16b5
Ed-Visinheski <eduardo.cook-visinheski@city.ac.uk>
2025-11-22 18:22:41 +0100

    Added pgpGenerator Class

---
+++ Core/pub/pgpGenerator.cpp
@@ -0,0 +1,6 @@
+#include "pgpGenerator.h"
+
+PgpGenerator::PgpGenerator()
+{
+    std::cout<<"Core is linked and compiling"<<std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "style.h"
+#include "pgpGenerator.h"
 
 int main()
 {
     Style style;
+    PgpGenerator pgpGenerator;
+    
     return 0;
 }
